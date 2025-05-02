0c95af2c9deb8086b3db5c8649db646532f93615
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-05-02 14:46:47 +0200

    llamaairforce : use latest as file name

--- src/llamaairforce.ts
+++ src/llamaairforce.ts
@@ -10,7 +10,7 @@ const main = async () => {
     const lastRound = rounds[rounds.length - 1]
 
     const { data } = await axios.get(`https://api.llama.airforce/bribes/votium/cvx-crv/${lastRound}`)
-    fs.writeFileSync(`./llamaairforce/${lastRound}.json`, JSON.stringify(data));
+    fs.writeFileSync(`./llamaairforce/latest.json`, JSON.stringify(data));
 };
 
 main().catch((error) => {
