e1c89ded710b06e53db089a6f401bf1c845669e2
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-05-16 13:01:14 +0200

    airforce add fxn

--- src/llamaairforce.ts
+++ src/llamaairforce.ts
@@ -4,13 +4,24 @@ import fs from 'fs';
 
 dotenv.config();
 
+const protocols = ["crv", "fxn"];
+const protocols_votemarket: Record<string, string> = {
+    "crv": "curve",
+    "fxn": "fxn"
+};
+
 const main = async () => {
-    const { data: { rounds } } = await axios.get('https://api.llama.airforce/bribes/votium/cvx-crv/rounds')
+    const datas: any = {};
 
-    const lastRound = rounds[rounds.length - 1]
+    for (const protocol of protocols) {
+        const { data: { rounds } } = await axios.get(`https://api.llama.airforce/bribes/votium/cvx-${protocol}/rounds`)
 
-    const { data } = await axios.get(`https://api.llama.airforce/bribes/votium/cvx-crv/${lastRound}`)
-    fs.writeFileSync(`./llamaairforce/latest.json`, JSON.stringify(data));
+        const lastRound = rounds[rounds.length - 1]
+
+        const { data } = await axios.get(`https://api.llama.airforce/bribes/votium/cvx-${protocol}/${lastRound}`)
+        datas[protocols_votemarket[protocol]] = data;
+    }
+    fs.writeFileSync(`./llamaairforce/latest.json`, JSON.stringify(datas));
 };
 
 main().catch((error) => {
