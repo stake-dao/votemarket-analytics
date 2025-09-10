bcaf3054dc21c3ed93daa1456197c94747ea7730
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-09-10 12:19:16 +0200

    fix vlcvx end timestamp

---
+++ src/endtimestamp.ts
@@ -0,0 +1,35 @@
+import fs from "fs"
+
+const WEEK = 604800;
+
+const main = async () => {
+    for(const protocol of ["curve", "fxn"]) {
+        const path = `./analytics/votemarket-analytics/vlcvx/${protocol}`
+        const pathRoundsMetadata = `${path}/rounds-metadata.json`
+        let roundsMetadata = JSON.parse(fs.readFileSync(pathRoundsMetadata, {encoding: 'utf-8'}));
+        
+        // Remove one week on each endVoting
+        roundsMetadata.map((round) => {
+            round.endVoting -= WEEK
+            return round
+        });
+
+        fs.writeFileSync(pathRoundsMetadata, JSON.stringify(roundsMetadata));
+
+        // Gauges files
+        const gaugeFiles = fs.readdirSync(`${path}/gauges`);
+        for(const gaugeFile of gaugeFiles) {
+            const pathGaugeFile = `${path}/gauges/${gaugeFile}`;
+            const gaugeFileData = JSON.parse(fs.readFileSync(pathGaugeFile, {encoding: 'utf-8'}))
+
+            for(const r of gaugeFileData) {
+                r.roundDetails.endTimestamp -= WEEK
+            }
+
+            fs.writeFileSync(pathGaugeFile, JSON.stringify(gaugeFileData));
+
+        }
+    }
+}
+
+main().catch((e) => console.log(e));
